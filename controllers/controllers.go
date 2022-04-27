0b8303cf6d1e1bc362526046cb61336fde52d282
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-04-27 13:44:19 -0300

    bug ao enviar arquivo

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -10,10 +10,21 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/matdorneles/leitor_csv/database"
 	"github.com/matdorneles/leitor_csv/models"
 )
+
+//função para verificar se há algum campo vazio
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
 
 //variável apontando pasta de templates html
 var temp = template.Must(template.ParseGlob("templates/*.html"))
@@ -28,10 +39,10 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-//lê o arquivo CSV e o retorna em linhas JSON
 func UploadArquivo(w http.ResponseWriter, r *http.Request) {
-	// Tamanho máximo do arquivo = 10mb
-	r.ParseMultipartForm(10 << 20)
+
+	//lê o arquivo CSV e o retorna em linhas JSON
+	r.ParseMultipartForm(10 << 20) // recebe arquivo enviado do HTML, tamanho máximo do arquivo = 10mb
 
 	// Handler para nome do arquivo, tamanho, header
 	file, handler, err := r.FormFile("arquivo")
@@ -42,7 +53,7 @@ func UploadArquivo(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
-	// Verificando se o arquivo não está vazio, um CSV vazio possui 2 bytes
+	// Verificando se o arquivo não está vazio, um CSV vazio possui ~2 bytes
 	var verificarSize bytes.Buffer
 	tamanhoArquivo, err := verificarSize.ReadFrom(file)
 	tamanhoArquivoConv := float64(tamanhoArquivo)
@@ -56,26 +67,21 @@ func UploadArquivo(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Header: %+v\n", handler.Header)
 
 	// Criando arquivo
-	csv, err := os.Create(handler.Filename)
+	fileCsv, err := os.Create(handler.Filename)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	// Copiando arquivo do upload para o criado no sistema
-	if _, err := io.Copy(csv, file); err != nil {
+	if _, err := io.Copy(fileCsv, file); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	fmt.Fprintf(w, "Arquivo enviado com sucesso!")
-	LerArquivo(string(handler.Filename))
 
-}
-
-// Lendo arquivo, separando linhas e virgulas e adicionando a cada atributo da classe Transacao
-func LerArquivo(arquivo string) {
-	arquivoCsv, err := os.Open(arquivo)
+	arquivoCsv, err := os.Open(handler.Filename)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -86,27 +92,48 @@ func LerArquivo(arquivo string) {
 	var transacoes []models.Transacao
 
 	for {
-		linha, err := leitor.Read()
-		if err == io.EOF {
-			break
-		} else if err != nil {
+		dadosCSV, err := leitor.ReadAll()
+		if err != nil {
 			log.Fatal(err)
 		}
 
-		transacoes = append(transacoes, models.Transacao{
-			BancoOrigem:       linha[0],
-			AgenciaOrigem:     linha[1],
-			ContaOrigem:       linha[2],
-			BancoDestino:      linha[3],
-			AgenciaDestino:    linha[4],
-			ContaDestino:      linha[5],
-			ValorTransacao:    linha[6],
-			DataHoraTransacao: linha[7],
-		})
+		//verificando data da primeira linha
+		dtPrimeiraTransacao, err := time.Parse("2006-01-02T15:04:05", dadosCSV[0][7])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		//lendo linha por linha e guardando dados para o DB
+		for _, linha := range dadosCSV {
+
+			if contains(linha, "") {
+				continue
+			}
+
+			dtTransacao, err := time.Parse("2006-01-02T15:04:05", linha[7])
+			if err != nil {
+				continue
+			}
+
+			if dtTransacao.Day() != dtPrimeiraTransacao.Day() && dtTransacao.Month() != dtPrimeiraTransacao.Month() && dtTransacao.Year() != dtPrimeiraTransacao.Year() {
+				continue
+			}
+
+			transacoes = append(transacoes, models.Transacao{
+				BancoOrigem:       linha[0],
+				AgenciaOrigem:     linha[1],
+				ContaOrigem:       linha[2],
+				BancoDestino:      linha[3],
+				AgenciaDestino:    linha[4],
+				ContaDestino:      linha[5],
+				ValorTransacao:    linha[6],
+				DataHoraTransacao: dtTransacao,
+			})
+		}
+		transacaoJson, _ := json.Marshal(transacoes)
+		fmt.Println(string(transacaoJson))
+
+		database.DB.Create(&transacoes)
 	}
-
-	transacaoJson, _ := json.Marshal(transacoes)
-	fmt.Println(string(transacaoJson))
-
-	database.DB.Create(&transacoes)
 }
--- models/models.go
+++ models/models.go
@@ -1,12 +1,14 @@
 package models
 
+import "time"
+
 type Transacao struct {
-	BancoOrigem       string `json:"banco-origem" validate:"nonzero"`
-	AgenciaOrigem     string `json:"agencia-origem" validate:"nonzero"`
-	ContaOrigem       string `json:"conta-origem" validate:"nonzero"`
-	BancoDestino      string `json:"banco-destino" validate:"nonzero"`
-	AgenciaDestino    string `json:"agencia-destino" validate:"nonzero"`
-	ContaDestino      string `json:"conta-destino" validate:"nonzero"`
-	ValorTransacao    string `json:"valor-transacao" validate:"nonzero"`
-	DataHoraTransacao string `json:"data-hora-transacao" validate:"nonzero"`
+	BancoOrigem       string    `json:"banco-origem"`
+	AgenciaOrigem     string    `json:"agencia-origem"`
+	ContaOrigem       string    `json:"conta-origem"`
+	BancoDestino      string    `json:"banco-destino"`
+	AgenciaDestino    string    `json:"agencia-destino"`
+	ContaDestino      string    `json:"conta-destino"`
+	ValorTransacao    string    `json:"valor-transacao"`
+	DataHoraTransacao time.Time `json:"data-hora-transacao"`
 }
